94956aea217ebb1738508e805c7c74fe51ada46a
Brandon Vo <brtroungvo@gmail.com>
2023-09-08 20:45:37 -0700

    Edit main.py and .gitignore

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #import required dependities
 import discord
 from discord.ext import commands
+from autocorrect import Speller
 
 #import Bot Token
 from tokens import *
 
+#instantiate Speller object
+text = Speller('en')
 
 intents = discord.Intents.all()
 
@@ -20,25 +23,23 @@ async def on_ready():
     print("-------------------------------------")
 
 
-#says hello
+#gives commands and how to use them
 @client.command()
 async def hello(ctx):
-    await ctx.send("Hello, I am SpellChecker. My prefix is ? and some of my commands are hello and spellCheck")
+    try:
+        await ctx.send("```\nHello, I am SpellChecker. My prefix is ? and my commands are help and spellcheck.\nFormat for spellcheck is:\n?spellcheck \" \"\n```")
+    except:
+        print("An exception occured")
 
 
 #given text, it will check the spelling
 @client.command()
-async def spellcheck(ctx, arg):
-    await ctx.send(arg.checker)
-
-
-#spell checks the paragraph and sends it back
-def checker(self, text):
-    self.result = 
+async def spellcheck(ctx, arg = "Missing argument"):
+    await ctx.send("```\n"+text(arg)+"\n```")
 
 
 #token goes in parentheses - dont give away very important
 client.run(botToken)
 
-# to add to server https://discord.com/api/oauth2/authorize?client_id=1147985792661213185&permissions=8&scope=bot
+# to add to server https://discord.com/api/oauth2/authorize?client_id=1149893802274869330&permissions=8&scope=bot
 
